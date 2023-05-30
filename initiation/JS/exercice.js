4df6ff620d8567815f3646a0c94540c39291b116
nawal <nbey34@gmail.com>
2023-05-30 16:50:29 +0200

    exercice javascript

---
+++ initiation/JS/exercice.js
@@ -0,0 +1,50 @@
+// var prenom="nawal";
+
+// var monNumero= 0616223;
+
+// var monNum= "0616223915";
+
+// var booleanfalse = false; // boolean
+
+// var booleantrue =true;// boolean
+
+// var varNULL = null; // null
+
+// console.log(prenom);
+// alert("voici mon prenom : nawal");
+// window.alert("Super, tu es arrivé sur mon site !");
+// document.write("A la pause vous aurez des Haribos Pik !");
+// window.prompt("Question: on est quel jour ?", "jour de la semaine");
+// prompt("Question: on est quel jour ?", "jour de la semaine");
+// var monPrenom = prompt();
+// document.write(monPrenom);
+
+// var unBooleen=false;//--true
+// console.log(unBooleen)
+// unBooleen="true";
+// console.log(unBooleen);
+// console.log(typeof unBooleen);
+// const PAYS="france";
+//  PAYS="italie";
+// document.write(PAYS);
+
+// var premierPrenom= window.prompt("cest quoi ton prenom");
+// document.write(premierPrenom);
+// console.log(premierPrenom);
+// alert(premierPrenom);
+
+// var nb1 = prompt();
+// console.log(typeof nb1);
+// nb1 = parseInt(nb1);
+// console.log(typeof nb1);
+// var nb2 = 7;
+// var resultat = nb1 % nb2;
+// resultat += nb2;
+// console.log(resultat);
+// alert(resultat);
+
+var ht = prompt();
+
+var ttc = ht * 1.2;
+
+document.write(ttc)
