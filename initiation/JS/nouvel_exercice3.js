32fc3dad2fb54b268746a3b1683c79620f689914
nawal <nbey34@gmail.com>
2023-06-07 11:46:28 +0200

    exercice 07/06/2023

---
+++ initiation/JS/nouvel_exercice3.js
@@ -0,0 +1,26 @@
+var affichagePrenoms = [
+  "faissal",
+  "alexis",
+  "cynthia",
+  "narci",
+  "wassila",
+  "michel",
+  "olivier",
+  "rahim",
+  "karima",
+  "alin",
+  "jeremy",
+  "nawal",
+];
+
+// function( affichagePrenoms);
+// 1-declarer(implanter) une fonction qui prend en parametre un tableau:
+// une fonction qui prend en parametre un tableau de 12 elements
+for (var i =0; i< namaffichagePrenoms.length; i++)[
+    document.write(namaffichagePrenoms[i])
+]
+
+// var affichagePrenoms = ["faissal", "alexis", "cynthia"];
+// for (var i = 12; i < affichagePrenoms.length; i++) {
+//   console.log(affichagePrenoms[i]);
+// }
