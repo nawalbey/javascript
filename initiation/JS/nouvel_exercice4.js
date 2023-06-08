75bd34db76bebdae43cb31ad7d9cf51a421634dd
nawal <nbey34@gmail.com>
2023-06-08 16:14:22 +0200

    exercice4

---
+++ initiation/JS/nouvel_exercice4.js
@@ -0,0 +1,27 @@
+// 1 implanter une fonction qui s'appelle " diabolique" qui prend un parametre "quelconque"
+
+//  2 la fonction aura une valeur de retour de type tableau
+
+//  2 le tableau declare dans la fonction sera rempli par les different parametres donnés
+
+// 3 nous allons executer la fonction 4 fois en lui passant les 4 type de variables: type tableau, string, number, boolean
+
+// 4 nous allons stocker la valeur de retour dans une variable " resultat"
+
+// 5 nous allons afficher la valeur de retour sur une page web et aussi a partir de la console
+
+// let resArray = [];
+
+// function diabolique(quelconque) {
+//   alert(quelconque);
+//   resArray[0] = quelconque;
+//   return resArray;
+// }
+
+// var valRetour = diabolique("narci");
+// console.log(valRetour);
+
+function additionDesCchiffres(number, string, tableau) {
+  for (var i = 0; i <= number; i++) var resultat;
+  return resultat;
+}
