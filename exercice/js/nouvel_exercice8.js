feb1da788f7d0422c4816d1c9f15a7cb799f542c
nawal <nbey34@gmail.com>
2023-06-20 10:59:16 +0200

    exercice 20.06.2023

---
+++ exercice/js/nouvel_exercice8.js
@@ -0,0 +1,49 @@
+// {/* <h1>Tournez manège</h1>
+
+// 	<ul>
+// 		<h2>Ecrivez un programme manege.js qui fait faire 10 tours de manège en affichant un message à chaque tour.</h2>
+
+// 		<li>C'est le tour de manège n°1</li>
+// 		<li>C'est le tour de manège n°2</li>
+// 		<li>C'est le tour de manège n°3</li>
+// 		<li>C'est le tour de manège n°4</li>
+// 		<li>C'est le tour de manège n°5</li>
+// 		<li>...</li>
+// 	</ul>
+
+// 	<h2>Ensuite, améliorez votre programme pour que le nombre de tours soit saisi par l'utilisateur.</h2>
+// 	<p>Testez avec des compteurs initialisés à d'autres valeurs que Zéro dans votre code.</p>
+
+// 	<h2>Résultat :</h2> */}
+
+// <h1>Nombre de jours du mois </h1>
+// <h2>Enoncé</h2>
+// <p>Ecrivez un programme qui fait saisir le numéro d'un mois (nombre entre 1 et 12) puis affiche le nombre de jours de ce mois.</p>
+// <p>On ne tiendra pas compte des années bissextiles.</p>
+// <p>Les erreurs de saisie doivent être gérées.</p>
+
+// <h2>Résultat :</h2>
+
+// ************exercie 1*********//
+var tour = parseInt(prompt("nombre de tour"));
+
+while ( manege <=tour) {
+    document.write("<p>c'est le tour de manége" + manege + "</p>");
+    manege++;
+}
+
+for ( manege=1; manege<=tour; manege++){
+    document.write("<p>c'est le tour de manége" + manege + "</p>");
+};
+
+
+
+// while (manege <= 10) {
+//   document.write("<p>c'est le tour de manége" + manege + "</p>");
+//   manege++;
+// }
+// console.log(manege);
+
+//*******ercice 2 *******//
+
+
