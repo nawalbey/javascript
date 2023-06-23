3827c2afd77997bc581186e52c729c1dff355d8d
nawal <nbey34@gmail.com>
2023-06-23 16:57:37 +0200

    javascript exerci 23.06.2023

---
+++ Exercices - Copie/16.js
@@ -0,0 +1,17 @@
+// alert('test');
+
+var disMoi = "";
+var i = 1;
+
+while ((disMoi !== "oui") && (disMoi !== "non") && (disMoi !== "Oui") && (disMoi !== "Non") && (disMoi !== "OUI") && (disMoi !== "NON")) {
+    disMoi = prompt("Allez!! On joue à ni oui ni non?");
+    document.write('"' + disMoi + '" n\'est pas une réponse, pour la ' + i + '° fois, veux-tu jouer oui ou non? <br>');
+    i++;
+}
+i--;//parce qu'il y a une incrémentation en trop
+document.write("<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " + i + " tours.</p>");
+
+var ouietnon =""
+var i = 1;
+
+while((ouietnon !=="oui")) && (ouietnon!=="non")
